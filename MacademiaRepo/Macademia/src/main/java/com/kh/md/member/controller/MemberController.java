98eb163c07ae9f89038cc6942ce31a7025650195
Attadipa <shining0172@gmail.com>
2022-10-14 21:43:03 +0900

    로그인 화면 수정

--- MacademiaRepo/Macademia/src/main/java/com/kh/md/member/controller/MemberController.java
+++ MacademiaRepo/Macademia/src/main/java/com/kh/md/member/controller/MemberController.java
@@ -2,12 +2,21 @@ package com.kh.md.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.kh.md.member.vo.MemberVo;
 
 @Controller
 public class MemberController {
 	
-	@GetMapping("/member/login")
+	@GetMapping("/")
 	public String login() {
 		return "member/login";
+	}
+	@PostMapping("/member/login")
+	public String login(MemberVo vo) {
+		
+		
+		return "home";
 	}
 }
---
+++ MacademiaRepo/Macademia/src/main/java/com/kh/md/member/vo/MemberVo.java
@@ -0,0 +1,5 @@
+package com.kh.md.member.vo;
+
+public class MemberVo {
+	
+}
