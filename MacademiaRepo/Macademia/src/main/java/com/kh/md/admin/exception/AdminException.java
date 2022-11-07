96c1c585bac6b8e66649e9e73a8627eef484b5a9
SEONGHYUN\Seonghyun <qkrtjdgus101@naver.com>
2022-11-07 22:22:41 +0900

    오류사항 임시조치

--- MacademiaRepo/Macademia/src/main/java/com/kh/md/admin/exception/AdminException.java
+++ MacademiaRepo/Macademia/src/main/java/com/kh/md/admin/exception/AdminException.java
@@ -1,10 +1,10 @@
-package com.kh.md.admin.exception;
-
-public class AdminException extends RuntimeException {
-
-	public AdminException() {}
-	
-	public AdminException(String msg) {
-		super(msg);
-	}
-}
+//package com.kh.md.admin.exception;
+//
+//public class AdminException extends RuntimeException {
+//
+//	public AdminException() {}
+//	
+//	public AdminException(String msg) {
+//		super(msg);
+//	}
+//}
